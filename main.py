a2cb1aea516162e1ba01d809e4e55db2df9345cf
daitomiun <mateo.rodriguez.fierro@gmail.com>
2024-12-24 12:04:42 -0500

    Create and draw player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,14 +10,25 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    max_fps = 60
+    clock = pygame.time.Clock()
+    dt = 0
+
+    p_x_position = SCREEN_WIDTH / 2
+    p_y_position = SCREEN_HEIGHT / 2
+    player = Player(p_x_position, p_y_position)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("blue")
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(max_fps) / 1000
+
 
 
 if __name__ == "__main__":
